900978204cacec60ff9f66e7fbb825569e2d2007
Sattar Naeem Ahmad <naeem@emergence.fi>
2017-06-21 12:15:34 +0300

    changes

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String [] args){
 	
 		for (int i=1;i<=10;i++){
-			System.out.println("Hello emergence from Naeem "+i);
+			System.out.println("Moi emergence "+i);
 		}
 	
 	}
